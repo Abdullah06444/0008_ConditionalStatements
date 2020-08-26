f4e9653c19a1938601a464f4a421a4fc35904938
Abdullah06444 <abdullah44gur@gmail.com>
2020-08-26 22:55:46 +0300

    Added CSharp Code

---
+++ CSharp0008_ConditionalStatements.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CSharp0008_ConditionalStatements
+{
+    class CSharp0008_ConditionalStatements
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("The monthly income is ");
+            int monthly_income = int.Parse(Console.ReadLine());
+            float monthly_tax;
+
+            if (monthly_income < 1000)
+            {
+
+                monthly_tax = (float)(0.0 * monthly_income);
+                Console.WriteLine("S/He will not pay any tax.");
+                Console.WriteLine(String.Format("{0:0.00}", monthly_tax) + " TL.");
+            }
+            else if (monthly_income >= 1000 && monthly_income < 3000)
+            {
+
+                monthly_tax = (float)(0.1 * monthly_income);
+                Console.Write("S/He should pay ");
+                Console.WriteLine(String.Format("{0:0.00}" , monthly_tax) + " TL.");
+            }
+            else
+            {
+
+                monthly_tax = (float)(0.15 * monthly_income);
+                Console.Write("S/He should pay ");
+                Console.WriteLine(String.Format("{0:0.00}", monthly_tax) + " TL.");
+            }
+
+            Console.WriteLine("You have to do it for living free in this country. :D");
+        }
+    }
+}
