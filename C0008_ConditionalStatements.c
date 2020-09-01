00aa45866567800e388e095c477d05f64e784a4a
Abdullah06444 <abdullah44gur@gmail.com>
2020-09-02 01:23:41 +0300

    Update C Code

--- C0008_ConditionalStatements.c
+++ C0008_ConditionalStatements.c
@@ -16,13 +16,13 @@ int main()
 		printf("%.2f TL.", monthly_tax);
 	}
 	else if (monthly_income >= 1000 && monthly_income < 3000) {
-		
+
 		monthly_tax = (0.1 * monthly_income);
 		printf("S/He should pay ");
 		printf("%.2f TL.", monthly_tax);
 	}
 	else {
-		
+
 		monthly_tax = (0.15 * monthly_income);
 		printf("S/He should pay ");
 		printf("%.2f TL.", monthly_tax);
